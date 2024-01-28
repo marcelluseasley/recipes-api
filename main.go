0a48b7229d44d50b5b5f005249c413898a596504
Marcellus Easley <marcellus.easley@gmail.com>
2024-01-28 02:15:54 -0500

    update recipe endpoint

--- main.go
+++ main.go
@@ -48,12 +48,34 @@ func ListRecipesHandler(c *gin.Context) {
 	
 }
 
+func UpdateRecipesHandler(c *gin.Context) {
+	id := c.Param("id")
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "recipe not found"})
+		return
+	}
+	recipes[index] = recipe 
+	c.JSON(http.StatusOK, recipe)
+}
+
 func main() {
 	router := gin.Default()
 
 
 	router.POST("/recipes", NewRecipesHandler)
 	router.GET("/recipes", ListRecipesHandler)
+	router.PUT("/recipes/:id", UpdateRecipesHandler)
 
 	router.Run()
 }
